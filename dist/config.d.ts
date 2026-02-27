e112b959d851f12eb4ba117d9304a5e30bb09be4
xzyqrn <herojaycalado@gmail.com>
2026-02-27 15:49:08 +0800

    update gitignore and remove dist from tracking

--- dist/config.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Config {
-    telegramBotToken: string;
-    openrouterApiKey: string;
-    openrouterModel: string;
-}
-export declare const config: Config;
-export declare function validateConfig(): void;
-//# sourceMappingURL=config.d.ts.map
